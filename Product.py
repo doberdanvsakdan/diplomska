3634de2c5cc478facabfc0ebeece4a688007994a
doberdanvsakdan <guppotf2@hotmail.com>
2023-04-10 12:05:55 +0200

    Dodan class Product

---
+++ Product.py
@@ -0,0 +1,9 @@
+class Product:
+    def __init__(self, ime_produkta, id):
+        self.id = id
+        self.ime_produkta = ime_produkta
+        self.kolicina = 1
+        self.dodaten_napajalnik = True
+        self.eu_napajalni_kabel = True
+        self.dodatni_stack_kabli = False
+
